40d783cc6bf593f7142fa8dee53d189b88a3c563
RoshanBaghwar <roshan.br800@gmail.com>
2024-06-13 19:08:23 +0530

    added html templates for Http Response

--- InstaBook/InstaBookApplication/views.py
+++ InstaBook/InstaBookApplication/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome To InstaBook!')
+    # return HttpResponse('<h1>Welcome To InstaBook!')
+    return render(request, 'index.html')
