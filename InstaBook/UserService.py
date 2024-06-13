02882e6f71dc837cfc0daad6b2d61032b7c9b3d8
RoshanBaghwar <roshan.br800@gmail.com>
2024-06-14 02:13:41 +0530

    created UserService

---
+++ InstaBook/UserService.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.http import HttpResponse
+from InstaBookApplication.models import Profile
+
+def signup(request):
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        mobile = request.POST['mobile']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(id=mobile).exists():
+                messages.info(request, 'Mobile Already Exists')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=name, id=mobile, email=email, password=password)
+                user.save()
+
+                user_model = User.objects.get(username=name)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
